9381b477dab9f32172d84e76482c65c8d3d46923
epicgamer5043 <137833264+epicgamer5043@users.noreply.github.com>
2023-06-27 14:29:40 -0600

    script.js

--- script.js
+++ script.js
@@ -1,137 +1,53 @@
-window.addEventListener('load', function() {
-  var canvas = document.getElementById('screenCanvas');
-  var ctx = canvas.getContext('2d');
-  var recordedActions = [];
-  var variables = {};
+let mediaRecorder;
+let recordedChunks = [];
+let recordedActions = [];
 
-  // Set canvas size to match the window size
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getDisplayMedia();
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
 
-  // Replicate screen movements
-  function replicateScreen() {
-    // Implement screen replication logic here
-    // Use recordedActions data and variables to replicate user interactions on the canvas
-    console.log('Replicating screen with variables:', variables);
+    document.getElementById('startBtn').disabled = true;
+    document.getElementById('stopBtn').disabled = false;
+  } catch (error) {
+    console.error('Error starting screen recording:', error);
   }
+};
 
-  // Record mouse movements
-  function recordMouseMovement(event) {
-    var action = {
-      type: 'mousemove',
-      x: event.clientX,
-      y: event.clientY,
-      timestamp: Date.now()
-    };
-    recordedActions.push(action);
+const stopRecording = () => {
+  mediaRecorder.stop();
+
+  document.getElementById('startBtn').disabled = false;
+  document.getElementById('stopBtn').disabled = true;
+  document.getElementById('replayBtn').disabled = false;
+};
+
+const handleDataAvailable = (event) => {
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
   }
+};
 
-  // Record mouse clicks
-  function recordMouseClick(event) {
-    var action = {
-      type: 'click',
-      x: event.clientX,
-      y: event.clientY,
-      timestamp: Date.now()
-    };
-    recordedActions.push(action);
-  }
-
-  // Capture user interactions
-  function captureInteractions() {
-    // Add event listeners to capture mouse movements and clicks
-    window.addEventListener('mousemove', recordMouseMovement);
-    window.addEventListener('click', recordMouseClick);
-  }
-
-  // Stop capturing user interactions
-  function stopCapture() {
-    // Remove event listeners for mouse movements and clicks
-    window.removeEventListener('mousemove', recordMouseMovement);
-    window.removeEventListener('click', recordMouseClick);
-  }
-
-  // Request user's screen capture permissions
-  navigator.mediaDevices
-    .getDisplayMedia({ video: true })
-    .then(function(stream) {
-      var videoElement = document.createElement('video');
-      videoElement.srcObject = stream;
-      videoElement.onloadedmetadata = function() {
-        videoElement.play();
-        // Draw the captured screen on the canvas for replication
-        setInterval(function() {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-          replicateScreen(); // Call the replication function
-        }, 100); // Adjust the interval as needed
-      };
-      captureInteractions(); // Start capturing user interactions
-    })
-    .catch(function(error) {
-      console.error('Error accessing screen capture:', error);
-    });
-
-  // Handle variable form submission
-  var variableForm = document.getElementById('variableForm');
-  variableForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    variables = {
-      variable1: document.getElementById('variable1').value,
-      variable2: document.getElementById('variable2').value
-      // Add more variables as needed
-    };
-    console.log('Variables applied:', variables);
-    enableControls(); // Enable controls after variables are applied
+const replayActions = () => {
+  recordedActions.forEach((action, index) => {
+    setTimeout(() => {
+      // Perform the recorded action
+      // For demonstration purposes, we'll log the action to the console
+      console.log(`Action ${index + 1}: ${action}`);
+    }, index * 1000); // Delay each action by 1 second
   });
+};
 
-  // Controls
-  var startButton = document.getElementById('startButton');
-  var stopButton = document.getElementById('stopButton');
-  var resetButton = document.getElementById('resetButton');
-
-  startButton.addEventListener('click', function() {
-    startReplication();
-    disableControls();
-  });
-
-  stopButton.addEventListener('click', function() {
-    stopReplication();
-    enableControls();
-  });
-
-  resetButton.addEventListener('click', function() {
-    resetReplication();
-    enableControls();
-  });
-
-  function enableControls() {
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    resetButton.disabled = true;
-  }
-
-  function disableControls() {
-    startButton.disabled = true;
-    stopButton.disabled = false;
-    resetButton.disabled = false;
-  }
-
-  function startReplication() {
-    // Start the replication process
-    console.log('Starting replication...');
-    // Use recordedActions and variables to replicate the task
-  }
-
-  function stopReplication() {
-    // Stop the replication process
-    console.log('Stopping replication...');
-    // Perform any necessary cleanup or stopping logic
-  }
-
-  function resetReplication() {
-    // Reset the replication process
-    console.log('Resetting replication...');
-    // Clear recordedActions and perform any necessary reset logic
-  }
+document.addEventListener('keydown', (event) => {
+  recordedActions.push(`Keydown: ${event.key}`);
 });
+
+document.addEventListener('click', (event) => {
+  recordedActions.push(`Click: (${event.clientX}, ${event.clientY})`);
+});
+
+document.getElementById('startBtn').addEventListener('click', startRecording);
+document.getElementById('stopBtn').addEventListener('click', stopRecording);
+document.getElementById('replayBtn').addEventListener('click', replayActions);
